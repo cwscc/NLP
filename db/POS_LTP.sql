0d72ea2f43f3e7369875efac5d92d844d2a6807d
cjq <653507470@qq.com>
2018-10-10 11:38:01 +0800

    LTP词性标注(创建新数据库)

--- db/POS_LTP.sql
+++ db/POS_LTP.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `postagprecessed` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `postagprecessed`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: postagprecessed
@@ -224,4 +226,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-09 19:47:17
+-- Dump completed on 2018-10-10 11:13:59
